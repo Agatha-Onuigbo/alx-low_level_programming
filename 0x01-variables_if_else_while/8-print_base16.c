3061ae8db1f1b9a9e20adeff97eafc1d8ed3e9e2
Agatha-Onuigbo <agathaonuigbo05@gmail.com>
2023-10-09 18:20:02 -0700

    print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	int m;
+
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
