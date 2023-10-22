68a45d442ae9ec551b2dc138542740e5a37d2dd4
Agatha-Onuigbo <agathaonuigbo05@gmail.com>
2023-10-22 12:54:22 -0700

    code prints a square and \n

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - prints a square to the terminal;
+ * @size: the number of ‘#’ to be used
+ * Return: void
+ */
+void print_square(int size)
+{
+	int j;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (j = 0; j < size; j++)
+		{
+			int i;
+
+			for (i = 0; i < size; i++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
