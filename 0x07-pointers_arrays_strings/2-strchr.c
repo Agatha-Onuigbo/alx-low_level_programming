f8494c26d04d8566a0ee1804bebd29fcd1250922
Agatha-Onuigbo <agathaonuigbo05@gmail.com>
2023-10-23 08:42:34 -0700

    code locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - finds a value within a string
+ * @s: this is the string to loop over
+ * @c: this is the value to find
+ * Return: address of the first match or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+
+	return (NULL);
+}
