91d9da0a4c221fe300dbdfff0ffde93f01a61c7b
Agatha_Onuigbo <agathaonuigbo05@gmail.com>
2024-06-29 23:18:39 +0100

    this file prints a script that writes a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
