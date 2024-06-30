2b3383a30e93fbb37790e085dcda532e547d3653
Agatha_Onuigbo <agathaonuigbo05@gmail.com>
2024-06-30 09:39:07 +0100

    a C program that prints exactly and that piece of art is useful followed by a new line to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar,", 47);
-	write(2, "2015-10-19\n", 12);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
