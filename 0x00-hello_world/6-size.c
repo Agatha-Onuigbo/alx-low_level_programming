57e2379b3c3dba4ccfff76788515666cf9eb7aa9
Agatha_Onuigbo <agathaonuigbo05@gmail.com>
2024-06-30 09:03:54 +0100

    this file prints a file that writes a C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: This function prints the size of various types on the computer 
+ * Description: This function prints the size of various types on the computer
  * it is compiled and run on.
  *
  * Return: Always 0 (Success)
